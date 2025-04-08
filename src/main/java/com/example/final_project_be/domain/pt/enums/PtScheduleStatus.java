ff681a9885992a8b6fd3c4c4321b474c109e3785
RJANG <150069439+R-JANG-98@users.noreply.github.com>
2025-04-08 16:22:58 +0900

    Fix: Status 관련 누락된 enum 추가

--- src/main/java/com/example/final_project_be/domain/pt/enums/PtScheduleStatus.java
+++ src/main/java/com/example/final_project_be/domain/pt/enums/PtScheduleStatus.java
@@ -3,6 +3,7 @@ package com.example.final_project_be.domain.pt.enums;
 public enum PtScheduleStatus {
     SCHEDULED,      // 예약됨
     COMPLETED,      // 완료됨
+    CHANGED,        // 변경됨
     CANCELLED,      // 취소됨
     NO_SHOW         // 불참
 } 
