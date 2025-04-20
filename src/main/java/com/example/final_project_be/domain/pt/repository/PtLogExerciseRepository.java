559cce42867f1c5ff5707cfd7a87ace1d05a8af6
jihxn-kim <lizars4024@gmail.com>
2025-04-20 17:47:29 +0900

    Feat: ptScheduleId 로 PtLogExercise 조회하는 API 추가

--- src/main/java/com/example/final_project_be/domain/pt/repository/PtLogExerciseRepository.java
+++ src/main/java/com/example/final_project_be/domain/pt/repository/PtLogExerciseRepository.java
@@ -1,7 +1,12 @@
 package com.example.final_project_be.domain.pt.repository;
 
-import com.example.final_project_be.domain.pt.entity.PtLogExercise;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.final_project_be.domain.pt.entity.PtLog;
+import com.example.final_project_be.domain.pt.entity.PtLogExercise;
+
 public interface PtLogExerciseRepository extends JpaRepository<PtLogExercise, Long> {
+    List<PtLogExercise> findByPtLogsOrderBySequenceAsc(PtLog ptLog);
 }
