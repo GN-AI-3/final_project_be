ec45e81c491cdd93b9b0bf56ed8dea9773ba8df8
jihxn-kim <lizars4024@gmail.com>
2025-04-21 15:38:26 +0900

    Fix: 보고서 최근 2회 조회 시 반환값 수정

--- src/main/java/com/example/final_project_be/domain/report/repository/ReportRepository.java
+++ src/main/java/com/example/final_project_be/domain/report/repository/ReportRepository.java
@@ -1,8 +1,14 @@
 package com.example.final_project_be.domain.report.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.final_project_be.domain.report.entity.Report;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    @Query("SELECT r FROM Report r WHERE r.ptContract.id = :ptContractId ORDER BY r.createdAt DESC")
+    List<Report> findTop2ByPtContractIdOrderByCreatedAtDesc(@Param("ptContractId") Long ptContractId);
 } 
